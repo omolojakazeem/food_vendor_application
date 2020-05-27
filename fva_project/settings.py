bd8709c818bc6d01c6ab929f6182a10e09153b2e
Kazeem Omoloja <omolojakazeem@gmail.com>
2020-05-27 04:53:48 +0100

    Fourth

--- fva_project/settings.py
+++ fva_project/settings.py
@@ -13,7 +13,7 @@ load_dotenv(verbose=True, dotenv_path=os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['vgg-fga.herokuapp.com']
 
